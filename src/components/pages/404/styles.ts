28326690d427a8415473ea8daccdacf2a45168bf
joaovictor3g <jvdias79797@gmail.com>
2023-06-18 15:43:28 -0300

    feat: adds 404 page

---
+++ src/components/pages/404/styles.ts
@@ -0,0 +1,73 @@
+import { styled } from "@/styles";
+
+export const NotFoundContainer = styled("div", {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  flexDirection: "column",
+  gap: "2rem",
+
+  height: "calc(100vh - 160px)",
+
+  fontFamily: "var(--inter)",
+
+  img: {
+    width: "100%",
+    objectFit: "contain",
+  },
+
+  ".text-content": {
+    display: "flex",
+    flexDirection: "column",
+    gap: "1rem",
+    alignItems: "center",
+
+    h1: {
+      fontWeight: 600,
+      fontSize: "1.75rem",
+      lineHeight: "34px",
+      color: "$gray20",
+    },
+
+    span: {
+      fontWeight: 400,
+      fontSize: "1.125rem",
+      lineHeight: "22px",
+      color: "$gray40",
+    },
+  },
+
+  ".back-home": {
+    color: "$primaryMain",
+    border: "1px solid currentColor",
+    backgroundColor: "transparent",
+    borderRadius: 4,
+    padding: "1rem 1.5rem",
+
+    fontSize: "$md",
+    fontStyle: "normal",
+    fontWeight: 500,
+    lineHeight: "17px",
+    transition: "background-color 0.2s, color 0.2s",
+    textDecoration: "none",
+
+    "&:hover": {
+      backgroundColor: "$primaryMain",
+      color: "$gray100",
+    },
+  },
+
+  "@md": {
+    padding: "0 1rem",
+
+    ".text-content": {
+      h1: {
+        fontSize: "$lg",
+      },
+
+      span: {
+        fontSize: "$md",
+      },
+    },
+  },
+});
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,26 @@
+import { NotFoundContainer } from "@/components/pages/404/styles";
+import Image from "next/image";
+
+import notFoundImg from "@/assets/404.png";
+import Head from "next/head";
+import Link from "next/link";
+
 export default function NotFound() {
-  return <div>404 not found</div>;
+  return (
+    <>
+      <Head>
+        <title>404 - Not Found</title>
+      </Head>
+      <NotFoundContainer>
+        <Image src={notFoundImg} alt="Conteúdo não encontrado" />
+        <div className="text-content">
+          <h1>Página não encontrada</h1>
+          <span>Não foi encontrada a página que você procura.</span>
+        </div>
+        <Link href="/" className="back-home">
+          Voltar a tela inicial
+        </Link>
+      </NotFoundContainer>
+    </>
+  );
 }
