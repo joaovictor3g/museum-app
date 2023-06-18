1ca46578c9a2820723120cb38e68051a853993ec
joaovictor3g <jvdias79797@gmail.com>
2023-06-18 13:33:46 -0300

    fix: fixes responsivity

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,21 +1,45 @@
 import { Wrapper } from "@/components/styled";
-import { HeaderContainer, Nav } from "./styles";
+import { HeaderContainer, MobileMenu, Nav, Overlay } from "./styles";
 import Link from "next/link";
 
 import logoImg from "@/assets/logo.svg";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { Menu, X } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export function Header() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   const { asPath } = useRouter();
 
+  function handleResize(e: any) {
+    if (e.target.innerWidth >= 768) {
+      setMobileMenuOpen(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    setMobileMenuOpen(false);
+  }, [asPath]);
+
   return (
-    <HeaderContainer>
+    <HeaderContainer className={mobileMenuOpen ? "mobile-menu-open" : ""}>
       <div className="fixed">
         <Wrapper className="header-wrapper">
           <Link href="/">
             <Image src={logoImg} alt="" />
           </Link>
+
+          <MobileMenu onClick={() => setMobileMenuOpen((current) => !current)}>
+            {mobileMenuOpen ? <X /> : <Menu />}
+          </MobileMenu>
 
           <Nav>
             <ul>
--- src/components/layout/Header/styles.ts
+++ src/components/layout/Header/styles.ts
@@ -3,6 +3,7 @@ import { styled } from "@/styles";
 export const HeaderContainer = styled("header", {
   background: "rgba(255, 255, 255, 0.005)",
   height: 80,
+  transition: "height 0.2s",
 
   ".fixed": {
     borderBottom: "1px solid $gray90",
@@ -22,9 +23,40 @@ export const HeaderContainer = styled("header", {
     justifyContent: "space-between",
   },
 
+  "&.mobile-menu-open": {
+    height: "300px",
+
+    ".fixed": {
+      height: "inherit",
+    },
+
+    nav: {
+      height: "220px",
+      display: "initial",
+      placeSelf: "middle",
+      gridColumnEnd: "span 2",
+
+      ul: {
+        width: "100%",
+        height: "inherit",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        textAlign: "center",
+
+        li: {
+          width: "100%",
+        },
+      },
+    },
+  },
+
   "@md": {
     ".header-wrapper": {
       padding: "1rem",
+      display: "grid",
+      gridTemplateColumns: "1fr 1fr",
     },
   },
 });
@@ -57,4 +89,27 @@ export const Nav = styled("nav", {
   "@md": {
     display: "none",
   },
+});
+
+export const MobileMenu = styled("button", {
+  backgroundColor: "transparent",
+  border: "none",
+  color: "$gray10",
+  justifySelf: "end",
+  lineHeight: 0,
+  display: "none",
+
+  "@md": {
+    display: "initial",
+  },
+});
+
+export const Overlay = styled("div", {
+  position: "fixed",
+  top: 0,
+  left: 0,
+  height: "100vh",
+  width: "100%",
+  background: "rgba(0,0,0,0.5)",
+  overflow: "hidden",
 });
