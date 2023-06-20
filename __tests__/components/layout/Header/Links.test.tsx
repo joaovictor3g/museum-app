6251f0a594f6bad59df2d87035b9f3fdbadfb6ca
joaovictor3g <jvdias79797@gmail.com>
2023-06-20 16:46:01 -0300

    test: adds logo tests

---
+++ __tests__/components/layout/Header/Links.test.tsx
@@ -0,0 +1,58 @@
+import { Links } from "@/components/layout/Header/Links";
+import { render, screen } from "@testing-library/react";
+import { describe, vi } from "vitest";
+import { useRouter } from "next/router";
+
+vi.mock("next/router", () => ({
+  useRouter: vi.fn(),
+}));
+
+describe("Header links", () => {
+  it("should render links correctly", () => {
+    (useRouter as any).mockReturnValue({ asPath: "/" });
+
+    render(<Links />);
+
+    const link1 = screen.getByTestId("home-link");
+    const link2 = screen.getByTestId("favorites");
+
+    expect(link1).toBeInTheDocument();
+    expect(link2).toBeInTheDocument();
+  });
+
+  it("should render link 'tela incial' active when user is in home", () => {
+    (useRouter as any).mockReturnValue({ asPath: "/" });
+
+    render(<Links />);
+
+    const link = screen.getByTestId("home-link");
+    expect(link).toHaveClass("active");
+  });
+
+  it("should render link 'favoritos' active when user is in favorites", () => {
+    (useRouter as any).mockReturnValue({ asPath: "/favorites" });
+
+    render(<Links />);
+
+    const link = screen.getByTestId("favorites");
+    expect(link).toHaveClass("active");
+  });
+
+  it("should not to render link 'tela incial' active when user is not in home", () => {
+    (useRouter as any).mockReturnValue({ asPath: "/favorites" });
+
+    render(<Links />);
+
+    const link = screen.getByTestId("home-link");
+    expect(link).not.toHaveClass("active");
+  });
+
+  it("should not to render link 'favoritos' active when user is not in favorites", () => {
+    (useRouter as any).mockReturnValue({ asPath: "/" });
+
+    render(<Links />);
+
+    const link = screen.getByTestId("favorites");
+    expect(link).not.toHaveClass("active");
+  });
+});
--- src/components/layout/Header/Links.tsx
+++ src/components/layout/Header/Links.tsx
@@ -22,6 +22,7 @@ export function Links() {
           <Link
             href="/favorites"
             className={asPath === "/favorites" ? "active" : ""}
+            data-testid="favorites"
           >
             meus favoritos
           </Link>
